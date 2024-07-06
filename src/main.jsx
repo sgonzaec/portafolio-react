081a59dccecf1b903789eb082cc9e669bf761883
Simon Gonzalez <simongonzalezechavarria@gmail.com>
2024-07-06 13:29:41 -0500

    feat: add analitycs

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import Background from './Components/Background/Background.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SpeedInsights/>
     <Background />
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
