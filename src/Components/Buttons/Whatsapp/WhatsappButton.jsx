424bad905930c01def6ffcd257b2e804d1a5e0ac
Simon Gonzalez <30642867+sgonzaec@users.noreply.github.com>
2024-09-09 18:37:37 -0500

    fix: update styles

--- src/Components/Buttons/Whatsapp/WhatsappButton.jsx
+++ src/Components/Buttons/Whatsapp/WhatsappButton.jsx
@@ -8,7 +8,7 @@ const WhatsappButton = () => {
             href={`https://wa.me/34613225846?text=${encodeURIComponent("Hola, Estoy interesado en que trabajemos juntos")}`}
             target="_blank"
             rel="noopener noreferrer"
-            className="inline-flex items-center px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition-colors duration-300"
+            className="flex justify-center items-center px-4 py-2 bg-green-500 text-white rounded-lg hover:bg-green-600 transition-colors duration-300"
         >
             {LanguageLogoByName('Whatsapp')} Contacta conmigo
         </a>
